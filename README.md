81310fbd415dd693262ba2313c018b1d9a528e84
Book Dragon <d.gonso@hotmail.com>
2019-07-20 14:45:28 -0700

    updated readme. added profile picture

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # techdegree-project6
+
+## Static Node and Express Site - Project Portfolio
+Portfolio showcase of past projects served using static node and express.
+
+## Getting Started
+Install the dependencies with `npm install`. Run project with `npm start`.
+
+## Dependencies
+JQuery
+Pug
+Express
+Node.js v10.16.0
+
+## Styling Personalization
+"Customize the style" extra credit
+
+- Changed the box shadow on .images so project photos would pop a bit more
+- Changed font color of "Learn More" link to be a bit more visible. Also updated hover color to contrast with non-hover color
+- Slightly increased font size of "Back" link and technologies list items
+- Added styling and friendly image for error page
+- Changed background color of the body
+- Added animation on the index page so the project landing images fade up one after the other
+
+## Other Features
+- Non-existent routes requested will display a friendly 404 error page
--- app.js
+++ app.js
@@ -7,26 +7,34 @@ const app = express();
 
 app.use("/static", express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
-
 app.use(router);
 
+// ROUTES
+// Index
 router.get('/', (req, res, next) => {
+    // Passed in data is used for creating and displaying project links and info
     res.render('index', {projects});
   });
 
+// About
 router.get('/about', (req, res, next) => {
     res.render('about');
 });
 
+// Projects
 router.get('/project/:id', (req, res) => {
+    // Store request project page id value...
     const {id} = req.params;
-
+    // ...and use it to pass the corresponding project object information for the template to display
     res.render("project", projects[id]);
 });
 
+// ERROR HANDLING
+// Non-existent path request creates a 404 error
 app.use((req, res, next) => {
     const err = new Error("Not Found");
     err.status = 404;
+    // Logs error to the console
     console.log("Sorry, we couldn't find the webpage you were looking for :( Error code:", err.status);
     next(err);
 });
@@ -34,6 +42,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
+    // Uses custom error template for user-friendly error display
     res.render("error");
 });
 
